3e337ca7ef09006e2f882f89d8e75b6f11183fa2
Troy Center <troycenter1@gmail.com>
2017-07-14 06:21:36 -0700

    updated routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Person = mongoose.model('Person'); // We are retrieving this Schema from our Models, named 'Person'
-const people = require('../controller/people.js')
+const people = require('../controller/people.js');
 
 module.exports = function(app) {
     app.get('/', function(req,res){
